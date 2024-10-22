c02232272eb28dc559acbcf359dbba31c76906c3
moth <74442751+Gincaza@users.noreply.github.com>
2024-10-22 23:11:02 +0100

    no código anterior caso o aluno mandasse uma nota maior que 20, entraria em um looping infinito

--- aula03/exercicio03.py
+++ aula03/exercicio03.py
@@ -1,13 +1,18 @@
 while True:
-    a = int(input("digite nota geografia: "))
-    b = int(input("digite a nota em mtm: "))
-    c = int(input("digite a nota em inglês: "))
-    d = int(input("digite a nota em português: "))
-    media = (a + b + c + d) / 4
-    if media >= 0 and media < 9.5:
-        print("reprovado")
-        break
-    elif media >= 9.5 and media <= 20:
-        print("aprovado")
+    a = int(input("Digite a nota de Geografia: "))
+    b = int(input("Digite a nota de Matemática: "))
+    c = int(input("Digite a nota de Inglês: "))
+    d = int(input("Digite a nota de Português: "))
+    
+    #verifica se as notas são válidas (entre 0 e 20)
+    if (0 <= a <= 20) and (0 <= b <= 20) and (0 <= c <= 20) and (0 <= d <= 20):
+        media = (a + b + c + d) / 4
+        
+        if media < 9.5:
+            print("Reprovado")
+            break
+        elif media >= 9.5:
+            print("Aprovado")
+            break
     else:
-        print("digite outra nota")
+        print("Digite outra nota válida (entre 0 e 20).")
