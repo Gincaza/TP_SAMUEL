cab1ea6c146ef884d4c3ebd3150ff86cb0bdbd35
moth <74442751+Gincaza@users.noreply.github.com>
2024-10-22 23:09:16 +0100

    Espaçamento pra elegibilidade

--- aula03/exercicio02.py
+++ aula03/exercicio02.py
@@ -2,5 +2,7 @@ xa = int(input("digite um número para xa: "))
 xb = int(input("digite um número para xb: "))
 ya = int(input("digite um número para ya: "))
 yb = int(input("digite um número para yb:  "))
+
 distancia_ab = (((xa-xb)**2 + (ya-yb)**2))**0.5
+
 print(distancia_ab)
