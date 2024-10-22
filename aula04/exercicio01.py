7a90b7eaf590670c1f7d0b4d2d81d1d7ca52e3b8
moth <74442751+Gincaza@users.noreply.github.com>
2024-10-22 23:21:36 +0100

    simplificar contagem de pares e ímpares e melhorar formatação de saída
    - Substituído 'par = par + 1' e 'impar = impar + 1' por '+=', tornando o código mais conciso.
    - Usado f-strings para melhorar a legibilidade da formatação de saída, tornando os prints mais claros e fáceis de entender.

--- aula04/exercicio01.py
+++ aula04/exercicio01.py
@@ -1,21 +1,17 @@
-
 valores_inteiros = []
 par = 0
 impar = 0
-for i in range(0,10):
-    valor = int(input("digite um número: "))
+
+for i in range(10):
+    valor = int(input("Digite um número: "))
     valores_inteiros.append(valor)
     if valor % 2 == 0:
-        par = par + 1
+        par += 1
     else:
-        impar = impar+1
+        impar += 1
 
-print(valores_inteiros)
+print(f"Lista de valores: {valores_inteiros}")
 diferenca = max(valores_inteiros) - min(valores_inteiros)
-print("a diferença entre o maior e menor é: ",diferenca)
-print("a quantidade de números pares é: ",par)
-print(" a quantidade de números ímpares é: ",impar)
-
-
-
-
+print(f"A diferença entre o maior e o menor número é: {diferenca}")
+print(f"A quantidade de números pares é: {par}")
+print(f"A quantidade de números ímpares é: {impar}")
